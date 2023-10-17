bebe4e3484302c987963953403efc949ee216647
Fiodar Shtytsko <shtytsko777@gmail.com>
2023-10-17 14:50:28 +0300

    init project

---
+++ MetalTest/MetalTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MetalTest
+//
+//  Created by Fiodar Shtytsko on 17/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
